c59574588d270e1a87a14b4088137dd3990af943
Vova Soroka <vsoroka@magecore.com>
2014-04-06 01:31:00 +0300

    BAP-3840: Development mode should not require cache clear

---
+++ Tests/Unit/Config/Loader/Fixtures/Bundle/TestBundle/TestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\CacheBundle\Tests\Unit\Config\Loader\Fixtures\Bundle\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestBundle extends Bundle
+{
+}
